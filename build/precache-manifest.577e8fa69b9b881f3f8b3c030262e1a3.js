0c2199f3c2293777d4b53943c9c3d318702690f9
vladimireanu <vladimireanu@gmail.com>
2020-05-20 14:42:49 +0300

    build

---
+++ build/precache-manifest.577e8fa69b9b881f3f8b3c030262e1a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7953658404af45f82f060b02a979e2cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73e7287201bd9dd3648b",
+    "url": "/static/css/main.99b2288b.chunk.css"
+  },
+  {
+    "revision": "e0fd7c27046d38405959",
+    "url": "/static/js/2.a5040bef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5040bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e7287201bd9dd3648b",
+    "url": "/static/js/main.ae14a41e.chunk.js"
+  },
+  {
+    "revision": "2a3c56c18a6598ae0793",
+    "url": "/static/js/runtime-main.b85da1a9.js"
+  }
+]);
